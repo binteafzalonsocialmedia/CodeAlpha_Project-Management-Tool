0cbd30f75cfb047a5ec3b2efc0cbf91d49f4ad3d
binteafzalonsocialmedia <binteafzal.on.socialmedia@gmail.com>
2026-08-26 17:27:28 +0500

    Initial project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className="text-3xl bg-green p-6 ">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
